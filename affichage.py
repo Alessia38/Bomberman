ea049400bfd6b70da8e45d5a9b277e6c9451fc64
survivor38 <jeremy.cupani@gmail.com>
2024-11-13 14:57:34 +0100

    Delete affichage.py

--- affichage.py
+++
@@ -1,8 +0,0 @@
-import variables_globales
-
-def afficher_plateau(plateau):
-    """Affiche le plateau de jeu."""
-    print(f"score :  {variables_globales.score}")
-    for ligne in plateau:
-        print(' '.join(ligne))
-        
